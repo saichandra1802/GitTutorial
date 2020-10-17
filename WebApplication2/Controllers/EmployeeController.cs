6559096339908c4c07c496b139284af90c802ce1
saichandra datrika <datrikasaichandra@gmail.com>
2020-10-17 20:17:19 +0530

    first commit

---
+++ WebApplication2/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EFTutorialEntities db = new EFTutorialEntities();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return Json(db.Employees.ToList(),JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApplication2/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            TestModel ts = new TestModel();
+            ts.Gender = "M";
+            ts.Name = "Test";
+            return View(ts);
+        }
+        [HttpPost]
+        public ActionResult save(TestModel ts)
+        {
+           
+            ts.Gender = "M";
+            ts.Name = "Test";
+            return View(ts);
+        }
+    }
+}
